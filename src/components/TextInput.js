31cea851519f7fac15f3b8f0f634921f89c53cd4
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-07-30 01:36:08 -0300

    Added logic to the text input

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import {React, useState} from 'react'
 
-function TextInput() {
-    return <input type="text"/>
+function TextInput({ onSend }) {
+    const [textValue, setTextValue] = useState("");
+
+    function onSubmit(e) {
+        e.preventDefault()
+        if(onSend) onSend(textValue);
+        setTextValue("");
+    }
+    
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input 
+                type="text"  
+                value={textValue}
+                onChange={(e) => setTextValue(e.target.value)}             
+            />
+            <input type="submit" hidden/>
+        </form>
+    )
 }
 
 export default TextInput;
