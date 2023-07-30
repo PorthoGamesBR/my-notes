5bd21971e6eaf8f567fec2816bef045ac139fd06
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-07-30 01:13:54 -0300

    Solved bug with directory
    components folder was created outside of src
    Moved folder to src
    Added to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import TextInput from './components/TextInput';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <TextInput />
     </div>
   );
 }
