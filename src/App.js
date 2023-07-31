714022629eced6c6067d4453cfa4b0b0238e3fd9
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-07-30 22:29:51 -0300

    Added noteList hook to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import TextInput from './components/TextInput';
 import Note from './components/Note/Note';
+
 import {useState} from "react"
+import useNoteList from './utils/CustomHooks/useNotes';
 import './App.css';
 
 function App() {
-  const [notes, setNotes] = useState([])
-
-  function onNewNoteSubmit(noteText) {
-    setNotes([...notes,noteText]);
-  }
+  const [notes, addNote] = useNoteList();
 
   return (
     <div className="App">
       <div>
         <h2>New Note</h2>
-        <TextInput onSend={onNewNoteSubmit}/>
+        <TextInput onSend={addNote}/>
       </div>
 
       {notes.map((n, index) => {
-        return <Note key={index} noteData={{text:n}}/>
+        return <Note key={index} noteData={n}/>
       })}
 
     </div>
