5578341659e8267f92f7544951075d54aaf5e4a9
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-08-03 23:25:47 -0300

    Added containers to App
    Added a Line Container as the base of the layout
    Wrapped Note List in Column Container
    Created Column Container for future rule list

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import TextInput from './components/TextInput';
 import Note from './components/Note/Note';
 import IconButton from './components/IconButton'
 import NoteHeader from './components/Note/NoteHeader'
+import LineContainer from './components/Containers/LineContainer';
+import ColumnContainer from './components/Containers/ColumnContainer';
 
 import {useState} from "react"
 import useNoteList from './utils/CustomHooks/useNotes';
+
 import './App.css';
 
 function App() {
@@ -16,15 +19,20 @@ function App() {
         <h2>New Note</h2>
         <IconButton icon={"✒"} onClick={() => addNote("New Note")} />
       </div>
-      
-      {notes.map((n) => {
-        return (
-        <div key={n.id}>
-          <NoteHeader onXClick={() => removeNote(n.id)} /> 
-          <Note noteData={n} onEdit={editNote}/>
-        </div>)
-      })}
-
+      <LineContainer>      
+        <ColumnContainer>
+          {notes.map((n) => {
+            return (
+            <div key={n.id}>
+              <NoteHeader onXClick={() => removeNote(n.id)} /> 
+              <Note noteData={n} onEdit={editNote}/>
+            </div>)
+          })}
+        </ColumnContainer>
+        <ColumnContainer>
+        {/* Add the rule list here */}
+        </ColumnContainer>
+      </LineContainer>
     </div>
   );
 }
