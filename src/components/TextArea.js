72ae50049821cfadb5d97ace1f8594be35c9d295
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-08-08 15:48:58 -0300

    Added ref to component to calculate height

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,19 +1,26 @@
-import {React, useState, useEffect} from "react"
+import {React, useState, useEffect, useRef} from "react"
 
 function TextArea({initValue, onSubmit}){
     const [textInput, setTextInput] = useState("");
     useEffect(()=>{setTextInput(initValue)},[initValue]);
 
+    const textAreaElm = useRef();
+
     function onKeyDown(e) {
-        if(e.keyCode == 13 && e.shiftKey == false){
+        calculateHeight();
+        if(e.keyCode === 13 && e.shiftKey === false){
             e.preventDefault();
             onSubmit(textInput);
         }
     }
 
+    function calculateHeight() {
+        console.log(textAreaElm.current.scrollHeight)
+    }
+
     return (
         <form>
-            <textarea onChange={(e) => setTextInput(e.target.value)} value={textInput} onKeyDown={onKeyDown}style={{resize:"none"}}/>
+            <textarea ref={textAreaElm} onChange={(e) => setTextInput(e.target.value)} value={textInput} onKeyDown={onKeyDown}style={{resize:"none"}}/>
             <input type="submit" hidden/>
         </form>
     )
