56afd7e286f6adc0e0901c4aa9a7e4cb7e940de1
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-08-08 16:06:47 -0300

    Added calculateLines function

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -7,15 +7,21 @@ function TextArea({initValue, onSubmit}){
     const textAreaElm = useRef();
 
     function onKeyDown(e) {
-        calculateHeight();
         if(e.keyCode === 13 && e.shiftKey === false){
             e.preventDefault();
             onSubmit(textInput);
         }
     }
 
+    // This function should return the target height for this element
     function calculateHeight() {
-        console.log(textAreaElm.current.scrollHeight)
+        return textAreaElm.current.scrollHeight;
+    }
+
+    function calculateLines(oneLineHeight, scrollHeight) {
+        const lines = Number.parseInt(scrollHeight / oneLineHeight);
+
+        return lines;
     }
 
     return (
