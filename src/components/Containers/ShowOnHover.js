beb8c558c717f0432722d5bc67d55fee268f8a96
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-08-17 21:58:40 -0300

    Removed unused function from ShowOnHover

--- src/components/Containers/ShowOnHover.js
+++ src/components/Containers/ShowOnHover.js
@@ -2,12 +2,7 @@ import { useState } from "react";
 
 function ShowOnHover({children}) {
     const [isHovered, setIsHovered] = useState(false);
-
-    function getStyleHovered() {
-        return "hidden";
-        return isHovered ? "block" : "hidden";
-    }
-
+    
     return (
     <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} style={{ minHeight: "0.8vh"}}>
         <div style={{display: isHovered ? "block" : "none"}}>
