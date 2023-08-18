6158e5551127138fb6f3132e3c22dc9ef1fcb9c6
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-08-18 08:59:58 -0300

    Added routes for React app[

--- server/index.js
+++ server/index.js
@@ -1,1 +1,34 @@
-process.on('exit', function() {console.log("Test")});
+const express = require("express");
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send("Hello World")
+});
+
+app.get('/app', (request, response) => {
+
+})
+
+app.get('/api/notes', (request, response) => {
+
+})
+
+app.post('/api/add', (request, response) = {
+
+})
+
+app.post('/api/delete', (request, response) = {
+
+})
+
+app.post('/api/edit', (request, response) = {
+
+})
+
+app.post('/api/order', (request, response) = {
+
+})
+
+
+app.listen(3000, () => {console.log("App available on http://localhost:3000")})
