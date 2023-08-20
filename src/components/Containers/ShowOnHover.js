73aa560bd479cf61e5d01f4649ac4fc9e27b9289
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-08-20 00:11:53 -0300

    Added check for mobile in ShowOnHover

--- src/components/Containers/ShowOnHover.js
+++ src/components/Containers/ShowOnHover.js
@@ -3,11 +3,23 @@ import { useState } from "react";
 function ShowOnHover({children}) {
     const [isHovered, setIsHovered] = useState(false);
     
+    const isTouchScreen = () => {
+        try{
+            document.createEvent("TouchEvent")
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+    
     return (
     <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} style={{ minHeight: "0.8vh"}}>
+        {!isTouchScreen() ? 
         <div style={{display: isHovered ? "block" : "none"}}>
             {children}
-        </div>
+        </div> : 
+        <>{children}</>}
     </div>)
 }
 
